f85e5b32ad2e1a94021da1e8fa0bee3402ccd3ca
UlrichMinkoulou <ulrichsolutions97@gmail.com>
2024-12-30 07:20:35 +0100

    'fc'

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Guten Tag!\nMein Name ist ULRICH MINKOULOU. Ich bin sechsundzwanzig jahre alt und bin Ingenieur von Beruf" << endl;
+    return 0;
+}
